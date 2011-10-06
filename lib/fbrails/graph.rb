653e9121f9f2ac77a0fcde731327fbfc2ab1406d
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-06 19:00:58 +0400

    get_picture

--- lib/fbrails/graph.rb
+++ lib/fbrails/graph.rb
@@ -4,9 +4,25 @@ module Fbrails
       @token = token
     end
 
-    def me
-      url = "https://graph.facebook.com/me/?access_token=#{@token}"
+    def get_object(obj)
+      url = "https://graph.facebook.com/#{obj}/?access_token=#{@token}"
       Fbrails.get(url)
     end
+
+
+    #graph.get_picture(ID,TYPE)
+    #TYPE can be square (50x50), small (50 pixels wide, variable height),  normal (100 pixels wide, variable height), large (about 200 pixels wide, variable height)
+    def get_picture(id,type=nil)
+      first = "http://graph.facebook.com/#{id}/picture"
+      if type.blank?
+        return first 
+      elsif type == 'square' || type = 'small' || type == 'normal' || type == 'large'
+        return first + "?type=#{type}"
+      end
+    end
+
+
+
+
   end
 end
