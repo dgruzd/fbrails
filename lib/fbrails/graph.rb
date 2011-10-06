9fd1c21dc3fb4e0f49e288ae8698a5ea60bf7424
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-06 22:19:47 +0400

    bug fixing, 0.0.9

--- lib/fbrails/graph.rb
+++ lib/fbrails/graph.rb
@@ -12,11 +12,11 @@ module Fbrails
 
     #graph.get_picture(ID,TYPE)
     #TYPE can be square (50x50), small (50 pixels wide, variable height),  normal (100 pixels wide, variable height), large (about 200 pixels wide, variable height)
-    def get_picture(id,type=nil)
+    def self.get_picture(id,type=nil)
       first = "http://graph.facebook.com/#{id}/picture"
       if type.blank?
         return first 
-      elsif type == 'square' || type = 'small' || type == 'normal' || type == 'large'
+      elsif type == 'square' || type == 'small' || type == 'normal' || type == 'large'
         return first + "?type=#{type}"
       end
     end
